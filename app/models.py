9be86a8a1169a2d81188f48b6be8692c51ad6125
kavvadasd <dimitris.sami@yahoo.gr>
2026-05-26 15:57:07 +0300

    Add user and borrow schemas + made some imporvements on database models

--- app/models.py
+++ app/models.py
@@ -2,6 +2,17 @@ from sqlalchemy import Column, Integer, String,ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from app.database import Base
 from datetime import datetime, timezone
+import enum
+
+
+class BookStatus(str, enum.Enum):
+    available = "available"
+    checked_out = "checked_out"
+
+
+class UserRole(str, enum.Enum):
+    user = "user"
+    admin = "admin"
 
 class Book(Base):
     __tablename__ = "books"
@@ -11,8 +22,8 @@ class Book(Base):
     author = Column(String, index=True, nullable=False)
     isbn = Column(String, unique=True, index=True, nullable=False)
     publication_year = Column(Integer, index=True, nullable=False)
-    status = Column(String, index=True, nullable=False, default="available")
-    borrow_records = relationship("BorrowRecord", back_populates="book")
+    status = Column(BookStatus,  nullable=False, default=BookStatus.available)
+    borrow_records = relationship("BorrowRecord", back_populates="book", cascade="all, delete-orphan")
 
 class User(Base):
     __tablename__ = "users"
@@ -21,7 +32,8 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    borrow_records = relationship("BorrowRecord", back_populates="user")
+    role = Column(UserRole, index=True, nullable=False, default=UserRole.user)
+    borrow_records = relationship("BorrowRecord", back_populates="user", cascade="all, delete-orphan")
 
 class BorrowRecord(Base):
     __tablename__ = "borrow_records"
@@ -29,7 +41,7 @@ class BorrowRecord(Base):
     id = Column(Integer, primary_key=True, index=True,nullable=False)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     book_id = Column(Integer, ForeignKey("books.id"), nullable=False)
-    borrow_date = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    borrow_date = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
     return_date = Column(DateTime, nullable=True)
 
     user = relationship("User", back_populates="borrow_records")
--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel, ConfigDict
 from enum import Enum
 from typing import Optional
-
+from datetime import datetime
+from pydantic import EmailStr
 class BookStatus(str, Enum):
     available = "available"
     checked_out = "checked_out"
@@ -11,7 +12,6 @@ class BookBase(BaseModel):
     author: str
     isbn: str
     publication_year: int
-    status: BookStatus
 
 class BookCreate(BookBase):
     pass
@@ -24,9 +24,56 @@ class BookUpdate(BaseModel):
     author: Optional[str] = None
     isbn: Optional[str] = None
     publication_year: Optional[int] = None
-    status: Optional[str] = None
+    status: Optional[BookStatus] = None
 
 class BookResponse(BookBase):
     id: int
     model_config = ConfigDict(from_attributes=True)
+
+class UserRole(str, Enum):
+    user = "user"
+    admin = "admin"
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+
+
+
+class UserCreate(UserBase):
+    password: str
+
+class UserResponse(UserBase):
+    id: int
+    role: UserRole  
+    model_config = ConfigDict(from_attributes=True)
+
+
+class BorrowRecordBase(BaseModel):
+    book_id: int
+
+
+class BorrowRecordCreate(BorrowRecordBase):
+    pass
+
+class BorrowRecordReturn(BorrowRecordBase):
+    return_date: datetime
+
+class BorrowRecordResponse(BorrowRecordBase):
+    id: int
+    user_id: int
+    book_id: int
+    borrow_date: datetime
+    return_date: Optional[datetime] = None
+    model_config = ConfigDict(from_attributes=True)
+
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer" #token_type is always bearer in this case
+
+
 
